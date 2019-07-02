6d1f1372c5f1c27c7a25fb42f54a55d44db8114f
WaiXinG <waixing.liew@gmail.com>
2019-07-02 22:48:38 +0800

    reset button created with function

--- src/main/scala/mummyMaze/Controller/GameOverController.scala
+++ src/main/scala/mummyMaze/Controller/GameOverController.scala
@@ -23,11 +23,13 @@ class GameOverController(
   }
 
   def gameOverSubmit(actionEvent: ActionEvent) = {
-    if(Main.playersHighScore.size < 6){
-      print("entered")
+    if(Main.playersHighScore.size < 5){
       val highScoreRecord = new HighScoreRecord(playerName.value,score)
       highScoreRecord.save()
       Main.playersHighScore += highScoreRecord
+
+      playersHighScore.sort((s,t) => s.score.value.v.value > t.score.value.v.value)
+
       Main.backHomePage()
     }else {
       var lowestScore = Main.playersHighScore.minBy( highscore => highscore.score.value.v.value)
