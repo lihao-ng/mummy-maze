83c27e6c1bde5f7a4710b6b0bd6a661b9c65e885
tsjie14 <tansj0314@gmail.com>
2019-07-02 15:10:15 +0800

    Change level to be btter

--- src/main/scala/mummyMaze/Levels/Level1.scala
+++ src/main/scala/mummyMaze/Levels/Level1.scala
@@ -1,75 +1,351 @@
-package mummyMaze.Maps
+package mummyMaze.Levels
 
 import scalafx.scene.paint.Color
 import scalafx.scene.shape.Rectangle
 
-object Level1 {
+object Levels {
     val color = Color.rgb(139, 69, 19)
 
-    val outerWall1 = Rectangle(700.0, 35.0)
-    outerWall1.fill = color
-
-    val outerWall2 = Rectangle(0.0, 665.0, 700.0, 35.0)
-    outerWall2.fill = color
-
-    val outerWall3 = Rectangle(35.0, 598.0)
-    outerWall3.fill = color
-
-    val outerWall4 = Rectangle(665.0, 0.0, 35.0, 700.0)
-    outerWall4.fill = color
-
-    val wall1 = Rectangle(152.0, 35.0, 35.0, 100.0)
-    wall1.fill = color
-
-    val wall2 = Rectangle(475.0, 35.0, 35.0, 100.0)
-    wall2.fill = color
-
-    val wall3 = Rectangle(276.0, 100.0, 200.0, 35.0)
-    wall3.fill = color
-
-    val wall4 = Rectangle(35.0, 218.0, 151.0, 35.0)
-    wall4.fill = color
-
-    val wall5 = Rectangle(282.0, 218.0, 147.0, 35.0)
-    wall5.fill = color
-
-    val wall6 = Rectangle(151.0, 253.0, 35.0, 100.0)
-    wall6.fill = color
-
-    val wall7 = Rectangle(394.0, 253.0, 35.0, 100.0)
-    wall7.fill = color
-
-    val wall8 = Rectangle(282.0, 353.0, 288.0, 35.0)
-    wall8.fill = color
-
-    val wall9 = Rectangle(35.0, 457.0, 269.0, 35.0)
-    wall9.fill = color
-
-    val wall10 = Rectangle(151.0, 492.0, 35.0, 100.0)
-    wall10.fill = color
-
-    val wall11 = Rectangle(533.0, 458.0, 35.0, 207.0)
-    wall11.fill = color
-
-    val wall12 = Rectangle(412.0, 458.0, 122.0, 35.0)
-    wall12.fill = color
-
-    val walls = List(
-      wall1,
-      wall2,
-      wall3,
-      wall4,
-      wall5,
-      wall6,
-      wall7,
-      wall8,
-      wall9,
-      wall10,
-      wall11,
-      wall12,
-      outerWall1,
-      outerWall2,
-      outerWall3,
-      outerWall4
+    val level1 = List(
+      new Rectangle {
+        x = 0.0
+        y = 0.0
+        width = 700.0
+        height = 35.0
+        fill = color
+      },
+      new Rectangle {
+        x = 0.0
+        y = 665.0
+        width = 700.0
+        height = 35.0
+        fill = color
+      },
+      new Rectangle {
+        x = 0.0
+        y = 0.0
+        width = 35.0
+        height = 598.0
+        fill = color
+      },
+      new Rectangle {
+        x = 665.0
+        y = 0.0
+        width = 35.0
+        height = 700.0
+        fill = color
+      },
+      new Rectangle {
+        x = 152.0
+        y = 35.0
+        width = 35.0
+        height = 100.0
+        fill = color
+      },
+      new Rectangle {
+        x = 475.0
+        y = 35.0
+        width = 35.0
+        height = 100.0
+        fill = color
+      },
+      new Rectangle {
+        x = 276.0
+        y = 100.0
+        width = 200.0
+        height = 35.0
+        fill = color
+      },
+      new Rectangle {
+        x = 35.0
+        y = 218.0
+        width = 151.0
+        height = 35.0
+        fill = color
+      },
+      new Rectangle {
+        x = 282.0
+        y = 218.0
+        width = 147.0
+        height = 35.0
+        fill = color
+      },
+      new Rectangle {
+        x = 151.0
+        y = 253.0
+        width = 35.0
+        height = 100.0
+        fill = color
+      },
+      new Rectangle {
+        x = 394.0
+        y = 253.0
+        width = 35.0
+        height = 100.0
+        fill = color
+      },
+      new Rectangle {
+        x = 282.0
+        y = 353.0
+        width = 288.0
+        height = 35.0
+        fill = color
+      },
+      new Rectangle {
+        x = 35.0
+        y = 457.0
+        width = 269.0
+        height = 35.0
+        fill = color
+      },
+      new Rectangle {
+        x = 151.0
+        y = 492.0
+        width = 35.0
+        height = 100.0
+        fill = color
+      },
+      new Rectangle {
+        x = 533.0
+        y = 458.0
+        width = 35.0
+        height = 207.0
+        fill = color
+      },
+      new Rectangle {
+        x = 412.0
+        y = 458.0
+        width = 122.0
+        height = 35.0
+        fill = color
+      }
     )
+    val level2 = List(
+    new Rectangle {
+      x = 0.0
+      y = 0.0
+      width = 700.0
+      height = 35.0
+      fill = color
+    },
+    new Rectangle {
+      x = 0.0
+      y = 665.0
+      width = 700.0
+      height = 35.0
+      fill = color
+    },
+    new Rectangle {
+      x = 0.0
+      y = 0.0
+      width = 35.0
+      height = 598.0
+      fill = color
+    },
+    new Rectangle {
+      x = 665.0
+      y = 0.0
+      width = 35.0
+      height = 700.0
+      fill = color
+    },
+    new Rectangle {
+      x = 105.0
+      y = 180.0
+      width = 250.0
+      height = 35.0
+      fill = color
+    },
+    new Rectangle {
+      x = 105.0
+      y = 100.0
+      width = 35.0
+      height = 80.0
+      fill = color
+    },
+    new Rectangle {
+      x = 212.0
+      y = 280.0
+      width = 35.0
+      height = 110.0
+      fill = color
+    },
+    new Rectangle {
+      x = 105.0
+      y = 454.0
+      width = 35.0
+      height = 100.0
+      fill = color
+    },
+    new Rectangle {
+      x = 247.0
+      y = 355.0
+      width = 108.0
+      height = 35.0
+      fill = color
+    },
+    new Rectangle {
+      x = 230.0
+      y = 565.0
+      width = 35.0
+      height = 100.0
+      fill = color
+    },
+    new Rectangle {
+      x = 450.0
+      y = 285.0
+      width = 35.0
+      height = 380.0
+      fill = color
+    },
+    new Rectangle {
+      x = 360.0
+      y = 504.0
+      width = 90.0
+      height = 35.0
+      fill = color
+    },
+    new Rectangle {
+      x = 485.0
+      y = 378.0
+      width = 90.0
+      height = 35.0
+      fill = color
+    },
+    new Rectangle {
+      x = 443.0
+      y = 106.0
+      width = 108.0
+      height = 35.0
+      fill = color
+    },
+    new Rectangle {
+      x = 550.0
+      y = 106.0
+      width = 35.0
+      height = 130.0
+      fill = color
+    },
+    new Rectangle {
+      x = 550.0
+      y = 234.0
+      width = 116.0
+      height = 35.0
+      fill = color
+    }
+  )
+    val level3 = List(
+    new Rectangle {
+      x = 0.0
+      y = 0.0
+      width = 700.0
+      height = 35.0
+      fill = color
+    },
+    new Rectangle {
+      x = 0.0
+      y = 665.0
+      width = 700.0
+      height = 35.0
+      fill = color
+    },
+    new Rectangle {
+      x = 0.0
+      y = 0.0
+      width = 35.0
+      height = 598.0
+      fill = color
+    },
+    new Rectangle {
+      x = 665.0
+      y = 0.0
+      width = 35.0
+      height = 700.0
+      fill = color
+    },
+    new Rectangle {
+      x = 188.0
+      y = 240.0
+      width = 108.0
+      height = 35.0
+      fill = color
+    },
+    new Rectangle {
+      x = 35.0
+      y = 563.0
+      width = 280.0
+      height = 35.0
+      fill = color
+    },
+    new Rectangle {
+      x = 153.0
+      y = 395.0
+      width = 35.0
+      height = 100.0
+      fill = color
+    },
+    new Rectangle {
+      x = 188.0
+      y = 460.0
+      width = 108.0
+      height = 35.0
+      fill = color
+    },
+    new Rectangle{
+      x = 153.0
+      y = 240.0
+      width = 35.0
+      height = 90.0
+      fill = color
+    },
+    new Rectangle {
+      x = 446.0
+      y = 510.0
+      width = 35.0
+      height = 155.0
+      fill = color
+    },
+    new Rectangle {
+      x = 530.0
+      y = 156.0
+      width = 35.0
+      height = 100.0
+      fill = color
+    },
+    new Rectangle {
+      x = 153.0
+      y = 124.0
+      width = 145.0
+      height = 35.0
+      fill = color
+    },
+    new Rectangle {
+      x = 437.0
+      y = 124.0
+      width = 128.0
+      height = 35.0
+      fill = color
+    },
+    new Rectangle {
+      x = 400.0
+      y = 330.0
+      width = 190.0
+      height = 35.0
+      fill = color
+    },
+    new Rectangle {
+      x = 480.0
+      y = 365.0
+      width = 35.0
+      height = 70.0
+      fill = color
+    },
+    new Rectangle {
+      x = 481.0
+      y = 510.0
+      width = 100.0
+      height = 35.0
+      fill = color
+    }
+  )
 }
